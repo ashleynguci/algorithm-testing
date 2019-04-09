dabf5801de6daa0dfa47f6d739ae71e051526f9a
ashleynguci <40316179+ashleynguci@users.noreply.github.com>
2019-04-09 23:21:55 +0300

    javascript-insertion-sort

---
+++ insertionSort.js
@@ -0,0 +1,18 @@
+function insertionSort(array) {
+  // change code below this line
+  var n = array.lenght;
+  for (var i = 1; i < n; i++) {
+    array[i] = key;
+    var j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = key;
+  }
+  // change code above this line
+  return array;
+}
+
+// test array:
+// [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
